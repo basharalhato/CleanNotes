87236544d8cacdb8dfccf0a659c86c93ae930bfd
Bashar Alhato <basharhato2001@gmail.com>
2021-10-20 09:03:24 +0300

    Implement AddNote use case

---
+++ app/src/main/java/dev/bhato/cleannotes/feature_note/domain/use_case/AddNote.kt
@@ -0,0 +1,21 @@
+package dev.bhato.cleannotes.feature_note.domain.use_case
+
+import dev.bhato.cleannotes.feature_note.domain.model.InvalidNoteException
+import dev.bhato.cleannotes.feature_note.domain.model.Note
+import dev.bhato.cleannotes.feature_note.domain.repository.NoteRepository
+
+class AddNote(
+    private val repository: NoteRepository
+) {
+
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note) {
+        if (note.title.isBlank()) {
+            throw InvalidNoteException("The title of the note cannot be empty.")
+        }
+        if (note.content.isBlank()) {
+            throw InvalidNoteException("The content of the note cannot be empty.")
+        }
+        repository.insertNote(note)
+    }
+}
