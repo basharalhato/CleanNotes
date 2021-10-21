b68f5f7c99c8cd7a0bd840e5a6dae20952c4c44a
Bashar Alhato <basharhato2001@gmail.com>
2021-10-21 09:10:27 +0300

    Edit navigation import

--- app/src/main/java/dev/bhato/cleannotes/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/dev/bhato/cleannotes/feature_note/presentation/MainActivity.kt
@@ -8,8 +8,8 @@ import androidx.compose.material.Surface
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import dagger.hilt.android.AndroidEntryPoint
 import dev.bhato.cleannotes.feature_note.presentation.add_edit_notes.AddEditNoteScreen
 import dev.bhato.cleannotes.feature_note.presentation.notes.NotesScreen
