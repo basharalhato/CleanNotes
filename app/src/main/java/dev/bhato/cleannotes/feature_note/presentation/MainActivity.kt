1d856cb843d1978e6612661cd8f341d27e3352ce
Bashar Alhato <basharhato2001@gmail.com>
2021-10-21 07:58:32 +0300

    Setup navigation

--- app/src/main/java/dev/bhato/cleannotes/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/dev/bhato/cleannotes/feature_note/presentation/MainActivity.kt
@@ -3,20 +3,66 @@ package dev.bhato.cleannotes.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
+import dev.bhato.cleannotes.feature_note.presentation.add_edit_notes.AddEditNoteScreen
+import dev.bhato.cleannotes.feature_note.presentation.notes.NotesScreen
+import dev.bhato.cleannotes.feature_note.presentation.util.Screen
 import dev.bhato.cleannotes.ui.theme.CleanNotesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CleanNotesTheme {
-                Surface(color = MaterialTheme.colors.background) {
-
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ) {
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                    "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
                 }
             }
         }
